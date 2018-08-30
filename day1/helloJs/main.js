f1280a44b6b6321db1d90dd7fac77efdfc331ccb
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-08-30 22:54:06 +0530

    js practice

---
+++ day1/helloJs/main.js
@@ -0,0 +1,38 @@
+
+// My first Program at AltCampus
+
+// variables --> var
+
+// var name = prompt("Enter your name");
+// var age = 45;
+// var college = true;
+
+// if (name == ""){
+//     alert(`Hello {);
+// }
+// else{
+//  alert(`Hello ${name} is ${age} in college ${college} `);
+// }
+
+
+// var num1 = prompt("");
+// var num2 = prompt("");
+// var op = prompt("");
+
+// if(op == "*"){
+//     alert(`$()`);
+// }else if (op == "-"){
+//     alert();
+// }else if (op == "+"){
+//     alert();
+// }else {
+//     alert();
+// }
+
+
+var b = prompt("");
+var c = prompt("");
+
+function calc() {
+    return console.log(b * c);
+}

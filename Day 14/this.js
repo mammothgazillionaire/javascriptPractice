031e670b868af309523947c1dd574af88ed33733
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-09-18 22:49:02 +0530

    Day 14 practice this

---
+++ Day 14/this.js
@@ -0,0 +1,146 @@
+console.log(this.document === document); // Output
+
+true
+
+
+console.log(this === window); //Output
+
+true
+
+var myFunction = function () {
+  console.log(this);
+};
+myFunction(); // Output
+
+Window object
+
+function f1() {
+  'use strict';
+  return this;
+}
+console.log(f1() === window); //Output
+
+false;
+
+
+function foo () {
+	console.log("Simple function call");
+	console.log(this === window); 
+}
+
+foo();	//Output  Simple function call , true
+console.log(this === window) // Output 
+
+Output  Simple function call , true
+
+true
+
+// This for IIFE
+(function(){
+	console.log("Anonymous function invocation");
+	console.log(this === window);
+})(); //Output
+
+Anonymous function invocation
+true
+
+// This for IIFE in strict mode
+function foo () {
+	'use strict';
+	console.log("Simple function call")
+	console.log(this === window); 
+}
+
+foo();// Output
+
+Simple function call
+
+false
+
+
+var myObject = {};
+myObject.someMethod = function () {
+  console.log(this);
+};
+myObject.someMethod(); // {someMethod: f()}
+
+
+// This refers to the New Instance
+
+function Person(fn, ln) {
+	this.first_name = fn;
+	this.last_name = ln;
+
+	this.displayName = function() {
+		console.log(`Name: ${this.first_name} ${this.last_name}`);
+	}
+}
+
+let person = new Person("John", "Reed");
+person.displayName();  // Name: John Reed
+let person2 = new Person("Paul", "Adams");
+person2.displayName();  // Name: Paul Adams
+
+
+
+
+
+//This refers to the invoker Object
+
+function foo () {
+	'use strict';
+	console.log("Simple function call")
+	console.log(this === window); 
+}
+
+let user = {
+	count: 10,
+	foo: foo,
+	foo1: function() {
+		console.log(this === window);
+	}
+}
+
+user.foo()  // Simple function call ,false
+let fun1 = user.foo1;
+fun1() // true
+user.foo1()  // false
+
+
+//this will call apply and bind
+
+
+this.x = 9;
+var module = {
+  x: 81,
+  getX: function() { return this.x; }
+};
+
+module.getX(); // 81
+
+var retrieveX = module.getX;
+retrieveX(); // 9
+
+var boundGetX = retrieveX.bind(module);
+boundGetX(); // 81
+
+
+// Call with new constructor
+
+
+function Person(fn, ln) {
+	this.first_name = fn;
+	this.last_name = ln;
+
+	this.displayName = function() {
+		console.log(`Name: ${this.first_name} ${this.last_name}`);
+	}
+}
+
+let person = new Person("John", "Reed");
+person.displayName(); // Name: John Reed
+
+let person2 = new Person("Paul", "Adams");
+person2.displayName(); // Name: Paul Adams
+
+person.displayName.call(person2); // Name: Paul Adams

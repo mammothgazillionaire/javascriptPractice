a7947b57f0aa1b2a3f27f4be0537e1a724f58f20
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-10-06 08:19:32 +0530

    collision canvas

--- Day 27/canvas/collision/script.js
+++ Day 27/canvas/collision/script.js
@@ -5,8 +5,8 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+  x: 10,
+  y: 10
 }
 
 var colors = [
@@ -36,7 +36,15 @@ function randomColor(colors){
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function Object(x,y,radius,color){
+function getDistance(x1,y1,x2,y2){
+  let xDistance = x2 - x1 ;
+  let yDistance = y2 - y1 ;
+
+  return Math
+  .sqrt(Math.pow(xDistance,2) + Math.pow(yDistance, 2));
+}
+
+function Circle(x,y,radius,color){
   this.x = x;
   this.y = y;
   this.radius = radius;
@@ -56,16 +64,30 @@ function Object(x,y,radius,color){
   };
 }
 
+let circle1;
+let circle2;
 function init(){
-  
+  circle1 = new Circle(300,300,100,'black');
+  circle2 = new Circle(10,10,30,'red');
 }
 
 function animate(){
   requestAnimationFrame(animate);
   ctx.clearRect(0,0, innerWidth,innerHeight);
   
-  
-  ctx.fillText("HTML CANVAS BOILERPLATE", mouse.x,mouse.y);
+  circle1.update();
+  circle2.x = mouse.x;
+  circle2.y = mouse.y;
+  circle2.update();
+
+  if (getDistance(circle1.x,circle1.y,circle2.x,circle2.y) < circle1.radius + circle2.radius){
+    circle1.color = 'red';
+  }else {
+    circle1.color = 'black';
+  }
+
+  console.log(getDistance(circle1.x,circle1.y,circle2.x,circle2.y));
+  // ctx.fillText("HTML CANVAS BOILERPLATE", mouse.x,mouse.y);
 }
 
 init();
